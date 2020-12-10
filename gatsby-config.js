5e2cbd2254df412ec06f226a9b4c53c8991c2b9e
Luca Gandini <luca.gandini88@gmail.com>
2020-12-11 00:10:57 +0100

    Updated metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,10 @@
 module.exports = {
   siteMetadata: {
     title: `Triplete.net`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Con Triplete.net il campionato di calcio italiano non avrà più segreti per te: controlla i risultati della Serie A quando vuoi, ovunque ti trovi. La tua squadra riuscirà a fare il triplete?`,
+    author: `@luca1388`,
+    image: 'https://triplete.net/preview.png',
+    url: 'https://triplete.net'
   },
   plugins: [
     {
@@ -26,13 +28,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Triplete.net`,
+        short_name: `Triplete`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        background_color: `#ffffff`,
+        theme_color: `#1482d2`,
+        display: `standalone`,
+        icon: `content/images/logo.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
