0bf11b5a026a7bd6d88aef116e25ec102a4996dd
Luca Gandini <luca.gandini88@gmail.com>
2020-12-15 18:52:48 +0100

    Improved floating filter

--- src/components/TeamFilter/TeamFilter.tsx
+++ src/components/TeamFilter/TeamFilter.tsx
@@ -38,9 +38,9 @@ const TeamFilter: React.FC<TeamFilterProps> = ({ onType }) => {
         alignItems: "center",
       }}
     >
-      <label className="select-label">Scegli squadra</label>
+      {/* <label className="select-label">Scegli squadra</label> */}
       <select className="select-team" style={{ margin: "0 0 0 10px" }} onChange={onChangeInputHandler}>
-        <option value={-1}>Tutte</option>
+        <option value={-1}>Tutte le squadre</option>
         {teams.map(team => (
           <option key={team.teamId} value={team.teamId}>
             {team.shortName}
