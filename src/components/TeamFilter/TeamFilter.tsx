2b6c69cb4548033e0871c7f8fd6a3139bfd29b05
Luca Gandini <luca.gandini88@gmail.com>
2020-12-15 21:21:00 +0100

    Teams name filtering sort by name

--- src/components/TeamFilter/TeamFilter.tsx
+++ src/components/TeamFilter/TeamFilter.tsx
@@ -20,7 +20,7 @@ const TeamFilter: React.FC<TeamFilterProps> = ({ onType }) => {
         }
       }
     }
-  `).allTeam.edges.map(edge => ({ ...edge.node }));
+  `).allTeam.edges.map(edge => ({ ...edge.node })).sort( (a, b) => a.shortName > b.shortName ? 1 : -1);
 
   const onChangeInputHandler = useCallback(
     (event: React.ChangeEvent<HTMLSelectElement>) => {
@@ -40,7 +40,7 @@ const TeamFilter: React.FC<TeamFilterProps> = ({ onType }) => {
     >
       {/* <label className="select-label">Scegli squadra</label> */}
       <select className="select-team" style={{ margin: "0 0 0 10px" }} onChange={onChangeInputHandler}>
-        <option value={-1}>Tutte le squadre</option>
+        <option value={-1} className="default-option">Tutte le squadre</option>
         {teams.map(team => (
           <option key={team.teamId} value={team.teamId}>
             {team.shortName}
