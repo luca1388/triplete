5e62a30d9574793eae64eac09c8cc3bf2c499ad8
Luca Gandini <luca.gandini88@gmail.com>
2020-12-28 16:10:21 +0100

    Google Analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,158 @@
+require("dotenv").config({
+  path: `.env`,
+});
+
+let plugins = [
+  {
+    resolve: `gatsby-plugin-typescript`,
+    options: {
+      isTSX: true, // defaults to false
+      jsxPragma: `jsx`, // defaults to "React"
+      allExtensions: true, // defaults to false
+    },
+  },
+  `gatsby-plugin-react-helmet`,
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `images`,
+      path: `${__dirname}/src/images`,
+    },
+  },
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      path: `${__dirname}/content/`,
+      name: "pages",
+    },
+  },
+  {
+    resolve: `gatsby-transformer-remark`,
+  },
+  {
+    resolve: `gatsby-source-rss-feed`,
+    options: {
+      url: `https://www.corrieredellosport.it/rss/calcio/serie-a`,
+      name: `NewsRSS`,
+    },
+  },
+  `gatsby-transformer-sharp`,
+  `gatsby-plugin-sharp`,
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: `Triplete.net`,
+      short_name: `Triplete`,
+      start_url: `/`,
+      icons: [
+        {
+          src: "content/images/icons/icon-48x48.png",
+          type: "image/png",
+          sizes: "48x48",
+        },
+        {
+          src: "content/images/icons/icon-72x72.png",
+          sizes: "72x72",
+          type: "image/png",
+        },
+        {
+          src: "content/images/icons/icon-96x96.png",
+          sizes: "96x96",
+          type: "image/png",
+        },
+        {
+          src: "content/images/icons/icon-128x128.png",
+          sizes: "128x128",
+          type: "image/png",
+        },
+        {
+          src: "content/images/icons/icon-144x144.png",
+          sizes: "144x144",
+          type: "image/png",
+        },
+        {
+          src: "content/images/icons/icon-152x152.png",
+          sizes: "152x152",
+          type: "image/png",
+        },
+        {
+          src: "content/images/icons/icon-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "content/images/icons/icon-256x256.png",
+          type: "image/png",
+          sizes: "256x256",
+        },
+        {
+          src: "content/images/icons/icon-384x384.png",
+          sizes: "384x384",
+          type: "image/png",
+        },
+        {
+          src: "content/images/icons/icon-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+        {
+          src: "content/images/icons/icon-512x512-maskable.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "any",
+        },
+      ],
+      background_color: `#ffffff`,
+      theme_color: `#1482d2`,
+      display: `standalone`,
+      icon: `content/favicon-32x32.png`, // This path is relative to the root of the site.
+      description:
+        "Con Triplete.net il campionato di calcio italiano non avrà più segreti per te: controlla i risultati della Serie A quando vuoi, ovunque ti trovi. La tua squadra riuscirà a fare il triplete?",
+      dir: "ltr",
+      lang: "it-IT",
+      scope: ".",
+      display: "standalone",
+      orientation: "portrait-primary",
+      shortcuts: [
+        {
+          name: "Marcatori",
+          short_name: "Marcatori",
+          description: "Classifica marcatori Serie A",
+          url: "/marcatori",
+          icons: [
+            { src: "content/images/icons/icon-96x96.png", sizes: "96x96" },
+          ],
+        },
+        {
+          name: "Partite",
+          short_name: "Partite",
+          description: "Calendario delle prossime partite di Serie A",
+          url: "/partite",
+          icons: [
+            { src: "content/images/icons/icon-96x96.png", sizes: "96x96" },
+          ],
+        },
+      ],
+    },
+  },
+  // this (optional) plugin enables Progressive Web App + Offline functionality
+  // To learn more, visit: https://gatsby.dev/offline
+  `gatsby-plugin-offline`,
+];
+
+if (process.env.GA_TRACKING_ID) {
+  plugins.push({
+    resolve: `gatsby-plugin-google-analytics`,
+    options: {
+      trackingId: process.env.GA_TRACKING_ID,
+      // Puts tracking script in the head instead of the body
+      head: false,
+      // Setting this parameter is optional
+      anonymize: true,
+    },
+  });
+}
+
 module.exports = {
   siteMetadata: {
     title: `Triplete.net`,
@@ -6,141 +161,5 @@ module.exports = {
     image: "https://triplete.net/preview.png",
     url: "https://triplete.net",
   },
-  plugins: [
-    {
-      resolve: `gatsby-plugin-typescript`,
-      options: {
-        isTSX: true, // defaults to false
-        jsxPragma: `jsx`, // defaults to "React"
-        allExtensions: true, // defaults to false
-      },
-    },
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/`,
-        name: "pages",
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-    },
-    {
-      resolve: `gatsby-source-rss-feed`,
-      options: {
-        url: `https://www.corrieredellosport.it/rss/calcio/serie-a`,
-        name: `NewsRSS`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Triplete.net`,
-        short_name: `Triplete`,
-        start_url: `/`,
-        icons: [
-          {
-            src: "content/images/icons/icon-48x48.png",
-            type: "image/png",
-            sizes: "48x48",
-          },
-          {
-            src: "content/images/icons/icon-72x72.png",
-            sizes: "72x72",
-            type: "image/png",
-          },
-          {
-            src: "content/images/icons/icon-96x96.png",
-            sizes: "96x96",
-            type: "image/png",
-          },
-          {
-            src: "content/images/icons/icon-128x128.png",
-            sizes: "128x128",
-            type: "image/png",
-          },
-          {
-            src: "content/images/icons/icon-144x144.png",
-            sizes: "144x144",
-            type: "image/png",
-          },
-          {
-            src: "content/images/icons/icon-152x152.png",
-            sizes: "152x152",
-            type: "image/png",
-          },
-          {
-            src: "content/images/icons/icon-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "content/images/icons/icon-256x256.png",
-            type: "image/png",
-            sizes: "256x256",
-          },
-          {
-            src: "content/images/icons/icon-384x384.png",
-            sizes: "384x384",
-            type: "image/png",
-          },
-          {
-            src: "content/images/icons/icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "content/images/icons/icon-512x512-maskable.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any",
-          },
-        ],
-        background_color: `#ffffff`,
-        theme_color: `#1482d2`,
-        display: `standalone`,
-        icon: `content/favicon-32x32.png`, // This path is relative to the root of the site.
-        description:
-          "Con Triplete.net il campionato di calcio italiano non avrà più segreti per te: controlla i risultati della Serie A quando vuoi, ovunque ti trovi. La tua squadra riuscirà a fare il triplete?",
-        dir: "ltr",
-        lang: "it-IT",
-        scope: ".",
-        display: "standalone",
-        orientation: "portrait-primary",
-        shortcuts: [
-          {
-            name: "Marcatori",
-            short_name: "Marcatori",
-            description: "Classifica marcatori Serie A",
-            url: "/marcatori",
-            icons: [
-              { src: "content/images/icons/icon-96x96.png", sizes: "96x96" },
-            ],
-          },
-          {
-            name: "Partite",
-            short_name: "Partite",
-            description: "Calendario delle prossime partite di Serie A",
-            url: "/partite",
-            icons: [
-              { src: "content/images/icons/icon-96x96.png", sizes: "96x96" },
-            ],
-          },
-        ],
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
-  ],
+  plugins: plugins,
 };
