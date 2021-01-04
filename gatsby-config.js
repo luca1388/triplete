ef75b188e8370cf8c1170c3bbfa0c73770e5abbc
Luca Gandini <luca.gandini88@gmail.com>
2021-01-04 17:11:41 +0100

    Fixed icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,65 +47,7 @@ let plugins = [
       background_color: `#ffffff`,
       theme_color: `#1482d2`,
       display: `standalone`,
-      icon: "content/images/icons/icon-512x512.png",
-      icons: [
-        {
-          src: "content/images/icons/icon-48x48.png",
-          type: "image/png",
-          sizes: "48x48",
-        },
-        {
-          src: "content/images/icons/icon-72x72.png",
-          sizes: "72x72",
-          type: "image/png",
-        },
-        {
-          src: "content/images/icons/icon-96x96.png",
-          sizes: "96x96",
-          type: "image/png",
-        },
-        {
-          src: "content/images/icons/icon-128x128.png",
-          sizes: "128x128",
-          type: "image/png",
-        },
-        {
-          src: "content/images/icons/icon-144x144.png",
-          sizes: "144x144",
-          type: "image/png",
-        },
-        {
-          src: "content/images/icons/icon-152x152.png",
-          sizes: "152x152",
-          type: "image/png",
-        },
-        {
-          src: "content/images/icons/icon-192x192.png",
-          sizes: "192x192",
-          type: "image/png",
-        },
-        {
-          src: "content/images/icons/icon-256x256.png",
-          type: "image/png",
-          sizes: "256x256",
-        },
-        {
-          src: "content/images/icons/icon-384x384.png",
-          sizes: "384x384",
-          type: "image/png",
-        },
-        {
-          src: "content/images/icons/icon-512x512.png",
-          sizes: "512x512",
-          type: "image/png",
-        },
-        {
-          src: "content/images/icons/icon-512x512-maskable.png",
-          sizes: "512x512",
-          type: "image/png",
-          purpose: "any",
-        },
-      ],
+      icon: "content/icon.png",
       description:
         "Con Triplete.net il campionato di calcio italiano non avrà più segreti per te: controlla i risultati della Serie A quando vuoi, ovunque ti trovi. La tua squadra riuscirà a fare il triplete?",
       dir: "ltr",
