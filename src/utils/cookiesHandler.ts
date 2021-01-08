0662ac3bcb52b7bf68391f450d4ab08179210b6a
Luca Gandini <luca.gandini88@gmail.com>
2021-01-09 00:32:12 +0100

    Fixed SSR for cookies

--- src/utils/cookiesHandler.ts
+++ src/utils/cookiesHandler.ts
@@ -1,5 +1,5 @@
 export const readCookie = (name: string) => {
-  if (!document) return;
+  if (typeof window === "undefined") return;
   const cookieString = document.cookie
     .split("; ")
     .find(row => row.startsWith(name));
@@ -11,14 +11,14 @@ export const setCookie = (
   value: string,
   expDays: number = 365
 ) => {
-  if (!document) return;
+  if (typeof window === "undefined") return;
   let d = new Date();
   d.setTime(d.getTime() + expDays * 24 * 60 * 60 * 1000);
   let expires = "expires=" + d.toUTCString();
   document.cookie = name + "=" + value + ";" + expires + ";path=/";
 };
 export const deleteCookie = (name: string) => {
-  if (!document) return;
+  if (typeof window === "undefined") return;
   document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 };
 
