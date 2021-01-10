f55b75a96b8e843c8ca1ffa07cb2cd05ce9751fa
Luca Gandini <luca.gandini88@gmail.com>
2021-01-11 00:08:48 +0100

    Testing PWA installation

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,37 @@
  * See: https://www.gatsbyjs.com/docs/browser-apis/
  */
 
-import './src/global.css';
+import "./src/global.css";
+
+// export const onServiceWorkerUpdateReady = () => {
+//   const answer = window.confirm(
+//     `This application has been updated. ` +
+//       `Reload to display the latest version?`
+//   );
+//   if (answer === true) {
+//     window.location.reload();
+//   }
+// };
+
+// // Wraps every page in a component
+// exports.wrapPageElement = ({ Element, props }) => {
+//     return <Element {...props} />;
+//   }
+
+window.addEventListener("DOMContentLoaded", () => {
+  let displayMode = "browser tab";
+  if (navigator.standalone) {
+    displayMode = "standalone-ios";
+  }
+  if (window.matchMedia("(display-mode: standalone)").matches) {
+    displayMode = "standalone";
+  }
+  // Log launch display mode to analytics
+  console.log("DISPLAY_MODE_LAUNCH:", displayMode);
+  alert("DISPLAY_MODE_LAUNCH:", displayMode);
+});
+
+window.addEventListener("appinstalled", evt => {
+  console.log("PWA installed");
+  alert("PWA installed");
+});
