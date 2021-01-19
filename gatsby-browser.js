af5374049b92defa21b6b4730e5cfed79c6d093f
Luca Gandini <luca.gandini88@gmail.com>
2021-01-19 17:58:23 +0100

    Update service worker on route change

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,10 +3,8 @@
  *
  * See: https://www.gatsbyjs.com/docs/browser-apis/
  */
-import { useGoogleAnalytics } from './src/hooks/useGoogleAnalytics';
+import { useGoogleAnalytics } from "./src/hooks/useGoogleAnalytics";
 import "./src/global.css";
-
-// export const onServiceWorkerUpdateReady = () => window.location.reload();
 
 // export const onServiceWorkerUpdateReady = () => {
 //   const answer = window.confirm(
@@ -18,48 +16,16 @@ import "./src/global.css";
 //   }
 // };
 
-// // Wraps every page in a component
-// exports.wrapPageElement = ({ Element, props }) => {
-//     return <Element {...props} />;
-//   }
-
-// window.addEventListener("DOMContentLoaded", () => {
-//   let displayMode = "browser tab";
-//   if (navigator.standalone) {
-//     displayMode = "standalone-ios";
-//   }
-//   if (window.matchMedia("(display-mode: standalone)").matches) {
-//     displayMode = "standalone";
-//   }
-//   // Log launch display mode to analytics
-//   console.log("DISPLAY_MODE_LAUNCH:", displayMode);
-//   alert("DISPLAY_MODE_LAUNCH:", displayMode);
-// });
-
-// window.addEventListener("load", async () => {
-//   const relatedApps = await navigator.getInstalledRelatedApps();
-//   console.log("installed");
-//   console.log(relatedApps);
-// });
-
-// document.onvisibilitychange(event => {
-//   console.log(event);
-//   let displayMode = "browser tab";
-//   if (navigator.standalone) {
-//     displayMode = "standalone-ios";
-//   }
-//   if (window.matchMedia("(display-mode: standalone)").matches) {
-//     displayMode = "standalone";
-//   }
-//   // Log launch display mode to analytics
-//   console.log("DISPLAY_MODE_LAUNCH:", displayMode);
-//   alert("DISPLAY_MODE_LAUNCH:", displayMode);
-// });
-
 window.addEventListener("appinstalled", evt => {
   const { fireEvent } = useGoogleAnalytics();
   fireEvent("Install", {
     event_category: "PWA",
-    event_label: "PWA installed"
+    event_label: "PWA installed",
   });
 });
+
+exports.onRouteUpdate = () => {
+  navigator.serviceWorker.register("/sw.js").then(reg => {
+    reg.update();
+  });
+};
