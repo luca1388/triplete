b881cc5ef202ddba392cf5d3356134104f98e36b
Luca Gandini <luca.gandini88@gmail.com>
2021-01-20 18:02:20 +0100

    Google ADSense support

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,8 +4,28 @@
  * See: https://www.gatsbyjs.com/docs/ssr-apis/
  */
 
-const React = require('react');
+const React = require("react");
 
-exports.onRenderBody = ({ setPostBodyComponents }, pluginOptions) => {
+let HeadComponents = [];
+
+if (
+  process.env.NODE_ENV === "production" &&
+  process.env.GATSBY_ADSENSE_CLIENT_ID &&
+  process.env.GATSBY_ADSENSE_CLIENT_ID !== " "
+) {
+  HeadComponents.push(
+    <script
+      data-ad-client={process.env.GATSBY_ADSENSE_CLIENT_ID}
+      async
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+    ></script>
+  );
+}
+
+exports.onRenderBody = (
+  { setHeadComponents, setPostBodyComponents },
+  pluginOptions
+) => {
   setPostBodyComponents([<div key="modal" id="modal-portal" />]);
+  setHeadComponents(HeadComponents);
 };
