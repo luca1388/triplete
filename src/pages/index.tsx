41defe4c6be1c959a9ce567764a0957967040b60
Luca Gandini <luca.gandini88@gmail.com>
2021-01-25 17:47:35 +0100

    Changed color for testing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -59,7 +59,7 @@ const IndexPage: React.FC<PageProps<IndexPageProps>> = ({ data }) => {
           alignItems: "center",
           flexDirection: "column",
           width: '100%',
-          backgroundColor: "rebeccapurple"
+          backgroundColor: "violet"
         }}
       >
         <Table standings={standings} />
