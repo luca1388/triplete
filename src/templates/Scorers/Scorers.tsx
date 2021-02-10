6f4ac3cabbed55fdcce14b8a592e36c67777e52c
Luca Gandini <lgandini@funambol.com>
2021-02-10 23:23:20 +0100

    Scorers filter improvements (no result message and close keyboard on enter press)

--- src/templates/Scorers/Scorers.tsx
+++ src/templates/Scorers/Scorers.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useRef, useState } from "react";
 import Layout from "../../components/Layout/Layout";
 import SEO from "../../components/SEO/seo";
 import Toolbar from "../../components/Toolbar/Toolbar";
@@ -10,7 +10,10 @@ import "./Scorers.css";
 const Filter = styled.input`
   font-size: 13px;
   padding: 0 5px;
-  max-width: 120px
+  max-width: 120px;
+`;
+const NoResultsText = styled.span`
+  font-style: italic;
 `;
 interface ScorersProps {
   pageContext: {
@@ -20,14 +23,30 @@ interface ScorersProps {
 
 const Scorers: React.FC<ScorersProps> = ({ pageContext }) => {
   const [filter, setFilter] = useState<string>("");
+  const filterInputRef = useRef<HTMLInputElement>(null);
 
   const getScorers = () => {
-    return filter ? pageContext.scorers.filter(
-      scorer =>
-        scorer.player.name.toLowerCase().indexOf(filter) > -1 ||
-        scorer.team.shortName.toLowerCase().indexOf(filter) > -1
-    ) : pageContext.scorers;
+    return filter
+      ? pageContext.scorers.filter(
+          scorer =>
+            scorer.player.name.toLowerCase().indexOf(filter) > -1 ||
+            scorer.team.shortName.toLowerCase().indexOf(filter) > -1
+        )
+      : pageContext.scorers;
   };
+
+  const changeFilterInputHandler = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    setFilter(event.target.value.toLowerCase());
+  }, []);
+
+  const keyDownHandler = useCallback(
+    (event: React.KeyboardEvent<HTMLInputElement>) => {
+      if (event.key === "Enter") {
+        filterInputRef.current?.blur();
+      }
+    },
+    [filterInputRef]
+  );
 
   return (
     <Layout defaultCompetition="SA">
@@ -38,30 +57,38 @@ const Scorers: React.FC<ScorersProps> = ({ pageContext }) => {
       <div className="scorersContainer">
         <Toolbar leagueName="Serie A">
           <Filter
+            ref={filterInputRef}
             type="text"
             value={filter}
             placeholder="Filtra per squadra"
-            onChange={event => setFilter(event.target.value.toLowerCase())}
+            onChange={changeFilterInputHandler}
+            onKeyDown={keyDownHandler}
           />
         </Toolbar>
-        {getScorers().map((entry, index) => {
-          const scorerClasses = ["scorer"];
-          if (index === 0) {
-            scorerClasses.push("firstScorer");
-          }
-          return (
-            <div className={scorerClasses.join(" ")} key={entry.id}>
-              <span className="number">{index + 1}</span>
-              <span className="playerName" style={{ flex: 1 }}>
-                <strong>{entry.player.name}</strong>{" "}
-                <span>({entry.team.shortName})</span>
-              </span>
-              <span className="number">
-                <strong>{entry.numberOfGoals}</strong>
-              </span>
-            </div>
-          );
-        })}
+        {getScorers().length ? (
+          getScorers().map((entry, index) => {
+            const scorerClasses = ["scorer"];
+            if (index === 0) {
+              scorerClasses.push("firstScorer");
+            }
+            return (
+              <div className={scorerClasses.join(" ")} key={entry.id}>
+                <span className="number">{index + 1}</span>
+                <span className="playerName" style={{ flex: 1 }}>
+                  <strong>{entry.player.name}</strong>{" "}
+                  <span>({entry.team.shortName})</span>
+                </span>
+                <span className="number">
+                  <strong>{entry.numberOfGoals}</strong>
+                </span>
+              </div>
+            );
+          })
+        ) : (
+          <NoResultsText>
+            Nessun risultato, prova a cercare altro ...
+          </NoResultsText>
+        )}
       </div>
     </Layout>
   );
